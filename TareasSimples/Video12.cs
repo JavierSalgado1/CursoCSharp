d4206accf57617e6f79aa89943ba3e3c7e12dd81
JavierSalgado1 <ja.salgadoa95@gmail.com>
2022-12-26 16:27:29 -0300

    Se agrega ejercicio de Video16

--- TareasSimples/Video12.cs
+++ TareasSimples/Video12.cs
@@ -24,6 +24,8 @@ namespace TareasSimples
             Console.Write("Tercer factor: ");
             num3 = ushort.Parse(Console.ReadLine()!);
 
+            // Se concatena el valor de los números ingresados con el resultado devuelto por el método Operación, en su 
+            // versión sobrecargada que recibe 3 parámetros.
             Console.WriteLine($"{num1} x {num2} x {num3} = " + Operacion(num1, num2, num3));
 
             Console.WriteLine("------ P O T E N C I A ------");
@@ -34,11 +36,15 @@ namespace TareasSimples
             Console.Write("Ingrese el exponente: ");
             num2 = ushort.Parse(Console.ReadLine()!);
             
+            // Se concatena el valor de los números ingresados con el resultado devuelto por el método Operación, en su 
+            // versión que recibe 2 parámetros.
             Console.WriteLine($"{num1} ^ {num2} = " + Operacion(num1, num2));
         }
 
         static uint Operacion(ushort num1, ushort num2)
         {
+            // El método Pow de la clase Math permite elevar el primer parámetro a la potencia del segundo parámetro, y como 
+            // devuelve un double, se realiza una conversión para almacenarlo en un tipo de dato uint.
             uint resultado = (uint)Math.Pow(num1, num2);
             return resultado;
         }
---
+++ TareasSimples/Video16.cs
@@ -0,0 +1,38 @@
+using System;
+// Realizar aplicación de consola que pida ingresar la talla de una camisa (S, M, L o XL), y que según la ingresada, imprima 
+// si tiene o no tiene descuento (las tallas L y XL lo tienen). Utilizar operadores lógicos.
+namespace TareasSimples
+{
+    class Video16
+    {
+        static void Main(string[] args)
+        {
+            string talla;
+
+            Console.WriteLine("Ingrese la talla de la camisa (S - M - L - XL): ");
+            talla = Console.ReadLine()!;
+
+            VerificaDescuento(talla);
+        }
+
+        static void VerificaDescuento(string talla)
+        {
+            // La siguiente instrucción en comentarios realiza la misma comprobación que el if finalmente utilizado, solo que 
+            // a través del método Compare de la clase String. Puede verse más complejo, pero es más flexible y permite realizar 
+            // una comprobación más flexible. El tercer parámetro indicado como false ordena que se compruebe que el texto a 
+            // comparar se encuentre tal cuál en cuanto a mayúsculas y minúsculas. Si colocamos true en vez de false, se puede 
+            // omitir esta parte de la comprobación, haciendo que, por ejemplo, "xl" sea lo mismo que "XL" a la hora de evaluar 
+            // el if.
+            //
+            // if (String.Compare(talla, "L", false) == 0 || String.Compare(talla, "XL", false) == 0)
+            if(talla == "L" || talla == "XL")
+            {
+                Console.WriteLine($"Esta camisa talla {talla} SÍ tiene descuento.");
+            }
+            else
+            {
+                Console.WriteLine($"Ests camisa talla {talla} NO tiene descuento.");
+            }
+        }
+    }
+}
