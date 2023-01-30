5afb0a263c003265c800348a57f24c1ed6530123
JavierSalgado1 <ja.salgadoa95@gmail.com>
2023-01-30 18:05:16 -0300

    Se agrega ejercicio Video74

---
+++ TareasParte4/Video74/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+
+namespace Video74
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ImprimirTablas(byte numero)
+        {
+            string tabla = "";
+
+            for(byte i = 1; i <= 15; i++)
+            {
+                tabla += $"{numero} x {i} = {(numero * i)}\n";
+            }
+
+            txtB_Salida.Text = tabla;
+        }
+
+        private void btn_Tabla3_Click(object sender, RoutedEventArgs e)
+        {
+            ImprimirTablas(3);
+        }
+
+        private void btn_Tabla7_Click(object sender, RoutedEventArgs e)
+        {
+            ImprimirTablas(7);
+        }
+
+        private void btn_Tabla10_Click(object sender, RoutedEventArgs e)
+        {
+            ImprimirTablas(10);
+        }
+    }
+}
---
+++ TareasParte4/Video74/README.md
@@ -0,0 +1,2 @@
+Realizar, por medio de las etiquetas XAML, una ventana de WPF que contenga 3 botones. Estos botones deberán desplegar la tabla del 
+3, 7 y 10 respectivamente en un textBlock.
