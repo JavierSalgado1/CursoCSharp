9ef21a31fbfc499b1105bec2e2594c34bb929f4e
JavierSalgado1 <ja.salgadoa95@gmail.com>
2023-02-22 21:44:04 -0300

    Se agrega ejercicio Video87

---
+++ TareasParte4/Video87/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+
+namespace Video87
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Eleccion_Click(object sender, RoutedEventArgs e)
+        {
+            byte validacion = ValidarSelecciones();
+
+            switch (validacion) 
+            {
+                case 0:
+                    string mensaje = "Instrumento: ";
+
+                    if ((bool)rdbtn_Guitarra.IsChecked) mensaje += "Guitarra\n";
+                    else mensaje += "Bajo\n";
+
+                    if ((bool)rdbtn_21.IsChecked) mensaje += "N° de trastes: 21";
+                    else if ((bool)rdbtn_22.IsChecked) mensaje += "N° de trastes: 22";
+                    else mensaje += "N° de trastes: 24";
+
+                    MessageBox.Show(mensaje);
+
+                    break;
+                case 1:
+                    MessageBox.Show("Debe seleccionar una guitarra o un bajo.");
+                    break;
+                case 2:
+                    MessageBox.Show("Debe seleccionar el número de trastes.");
+                    break;
+            }
+        }
+
+        private byte ValidarSelecciones()
+        {
+            if (!(bool)rdbtn_Guitarra.IsChecked && !(bool)rdbtn_Bajo.IsChecked) return 1;
+            if (!(bool)rdbtn_21.IsChecked && !(bool)rdbtn_22.IsChecked && !(bool)rdbtn_24.IsChecked) return 2;
+
+            return 0;
+        }
+    }
+}
---
+++ TareasParte4/Video87/README.md
@@ -0,0 +1,3 @@
+Crear formulario en WPF que permita elegir entre una guitarra y un bajo, y además, que permita elegir la cantidad de trastes del mismo 
+(21, 22 o 24). Estos dos campos deberán elegirse por medio de radio buttons independientes y finalmente, deberá mostrarse en mensaje 
+las opciones escogidas.
